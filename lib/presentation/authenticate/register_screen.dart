85cc0fb28cb93e6a22422e1817d468d40c24eb47
Manuel Aguilar <jorgemanuelaguilar4@gmail.com>
2025-03-07 15:14:39 -0600

    se modifico la pantalla levels_screen

--- lib/presentation/authenticate/register_screen.dart
+++ lib/presentation/authenticate/register_screen.dart
@@ -3,10 +3,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:mqh_rommel/controllers/auth_controller.dart';
 
-
-
-
-
 class RegisterScreen extends ConsumerStatefulWidget {
   @override
   _RegisterScreenState createState() => _RegisterScreenState();
--- lib/presentation/screens/levels_screen.dart
+++ lib/presentation/screens/levels_screen.dart
@@ -12,31 +12,34 @@ class LevelsScreen extends ConsumerStatefulWidget {
 }
 
 class _LevelsScreenState extends ConsumerState<LevelsScreen> {
-
   @override
   Widget build(BuildContext context) {
-     String imageProfile =
-         UtilsApp.cleanEmailUsername(ref.watch(authControllerProvider)!.email);
-
+    String imageProfile =
+        UtilsApp.cleanEmailUsername(ref.watch(authControllerProvider)!.email);
 
     return Scaffold(
-      extendBodyBehindAppBar: true, // Extiende el cuerpo detrás del AppBar
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        backgroundColor: Colors.transparent, // Hace el AppBar transparente
+        backgroundColor: Colors.transparent,
         elevation: 0,
         centerTitle: true,
         title: const Text(
           'NIVELES',
           style: TextStyle(
-            color: Colors.white, // Cambia el color del texto a blanco para contraste
+            color: Colors.white,
             fontSize: 20,
             fontWeight: FontWeight.bold,
           ),
         ),
-        actions:  [
+        actions: [
           Padding(
-            padding:const  EdgeInsets.only(right: 16.0),
-            child: UserAvatarImage(imageProfile: imageProfile, radius : 25, color: Colors.white, width: 1,),
+            padding: const EdgeInsets.only(right: 16.0),
+            child: UserAvatarImage(
+              imageProfile: imageProfile,
+              radius: 25,
+              color: Colors.white,
+              width: 1,
+            ),
           ),
         ],
       ),
@@ -51,68 +54,181 @@ class _LevelsScreenState extends ConsumerState<LevelsScreen> {
           ),
         ),
         child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                child: GridView.count(
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 16,
-                  crossAxisSpacing: 16,
-                  childAspectRatio: 1.2,
-                  children: [
-                    _buildLevel(
-                      title: 'Abecedario',
-                      subtitle: 'Aprendiendo las letras',
+          padding: EdgeInsets.only(
+            top: kToolbarHeight + 16, // Ajuste para respetar el AppBar
+            left: 16,
+            right: 16,
+            bottom: 16,
+          ),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _buildSection("Abecedario", [
+                  _buildFullWidthLevel(
+                      title: 'Nivel 1',
+                      subtitle: 'Aprendiendo el abecedario',
                       color: Colors.redAccent,
-                      icon: Icons.abc,
-                    ),
-                    _buildLevel(
-                      title: 'Números',
-                      subtitle: 'Aprendiendo los números 1-20,100',
+                      icon: Icons.abc),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: _buildLevel(
+                          title: 'Nivel 2',
+                          subtitle: 'Aprendiendo el abecedario',
+                          color: Colors.orange,
+                          icon: Icons.text_fields,
+                        ),
+                      ),
+                      const SizedBox(width: 16),
+                      Expanded(
+                        child: _buildLevel(
+                          title: 'Nivel 3',
+                          subtitle: 'Aprendiendo el abecedario',
+                          color: Colors.green,
+                          icon: Icons.text_fields,
+                        ),
+                      ),
+                    ],
+                  ),
+                ]),
+                _buildSection("Números", [
+                  _buildFullWidthLevel(
+                      title: 'Nivel 4',
+                      subtitle: 'Aprendiendo los números',
                       color: Colors.purpleAccent,
-                      icon: Icons.numbers,
-                    ),
-                    _buildLevel(
-                      title: 'Días de la semana',
-                      subtitle: 'Aprendiendo los días de la semana',
-                      color: Colors.grey,
-                      icon: Icons.weekend_sharp,
-                    ),
-                    _buildLevel(
-                      title: 'Meses',
-                      subtitle: 'Aprendiendo los meses del año',
-                      color: Colors.grey,
-                      icon: Icons.calendar_month,
-                    ),
-                    _buildLevel(
-                      title: 'Frases',
-                      subtitle: 'Dentro del salón',
-                      color: Colors.grey,
-                      icon: Icons.people,
-                    ),
-                    _buildLevel(
-                      title: 'Frases',
-                      subtitle: 'Preguntas escolares',
-                      color: Colors.grey,
-                      icon: Icons.people,
-                    ),
-                    _buildLevel(
-                      title: 'Frases',
-                      subtitle: 'Glosario de materias',
-                      color: Colors.grey,
-                      icon: Icons.people,
-                    ),
-                  ],
-                ),
-              ),
-            ],
+                      icon: Icons.numbers),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: _buildLevel(
+                          title: 'Nivel 5',
+                          subtitle: 'Aprendiendo los números',
+                          color: Colors.blue,
+                          icon: Icons.numbers,
+                        ),
+                      ),
+                      const SizedBox(width: 16),
+                      Expanded(
+                        child: _buildLevel(
+                          title: 'Nivel 6',
+                          subtitle: 'Aprendiendo los números',
+                          color: Colors.blueGrey,
+                          icon: Icons.numbers,
+                        ),
+                      ),
+                    ],
+                  ),
+                ]),
+                _buildSection("Calendario", [
+                  _buildFullWidthLevel(
+                      title: 'Nivel 7',
+                      subtitle: 'Aprendiendo el calendario',
+                      color: Colors.redAccent,
+                      icon: Icons.calendar_month),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: _buildLevel(
+                          title: 'Nivel 8',
+                          subtitle: 'Aprendiendo el calendario',
+                          color: Colors.orange,
+                          icon: Icons.calendar_month,
+                        ),
+                      ),
+                      const SizedBox(width: 16),
+                      Expanded(
+                        child: _buildLevel(
+                          title: 'Nivel 9',
+                          subtitle: 'Aprendiendo el calendario',
+                          color: Colors.green,
+                          icon: Icons.calendar_month,
+                        ),
+                      ),
+                    ],
+                  ),
+                ]),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
+
+  //widget hecho con ayuda manu
+
+  Widget _buildSection(String title, List<Widget> children) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const SizedBox(height: 16),
+        Text(
+          title,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(height: 8),
+        ...children,
+        const SizedBox(height: 16),
+      ],
+    );
+  }
+
+  //widget hecho con ayuda manu
+
+  Widget _buildFullWidthLevel({
+    required String title,
+    required String subtitle,
+    required Color color,
+    required IconData icon,
+  }) {
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      padding: const EdgeInsets.all(16),
+      margin: const EdgeInsets.only(bottom: 16),
+      child: Row(
+        children: [
+          Icon(icon, color: Colors.white.withOpacity(0.7), size: 40),
+          const SizedBox(width: 16),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  subtitle,
+                  style: TextStyle(
+                    color: Colors.white.withOpacity(0.8),
+                    fontSize: 14,
+                  ),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  //widget del profe
 
   Widget _buildLevel({
     required String title,
@@ -143,16 +259,14 @@ class _LevelsScreenState extends ConsumerState<LevelsScreen> {
             ),
           ),
           const SizedBox(height: 5),
-          Flexible(
-            child: Text(
-              subtitle,
-              style: TextStyle(
-                color: Colors.white.withOpacity(0.8),
-                fontSize: 14,
-              ),
-              maxLines: 2, // Limita el número de líneas a 2 para evitar desbordamiento
-              overflow: TextOverflow.ellipsis, // Agrega puntos suspensivos si el texto es muy largo
+          Text(
+            subtitle,
+            style: TextStyle(
+              color: Colors.white.withOpacity(0.8),
+              fontSize: 14,
             ),
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
           ),
         ],
       ),
